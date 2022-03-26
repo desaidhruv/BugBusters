c3deefc4026edef68924ca422a786d63958fd411
Dhruv Desai <dhruv@bhyve.io>
2022-03-27 03:44:28 +0530

    Map Added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
 import { DarkModeSwitch } from "../components/DarkModeSwitch";
+import Head from "next/head";
 
 const Index = () => (
   <>
+    <Head>
+      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAUCwY4Zz6_WdPTEMx_q0LxDqVkUpZxOWE" />
+    </Head>
     {/* <Header /> */}
     {/* <Flex/> */}
     {/* <DarkModeSwitch /> */}
---
+++ src/pages/map/index.jsx
@@ -0,0 +1,100 @@
+import React from "react";
+import {
+  GoogleMap,
+  useLoadScript,
+  Marker,
+  InfoWindow,
+} from "@react-google-maps/api";
+import { formatRelative } from "date-fns";
+
+import mapStyles from "./mapStyles";
+
+const libraries = ["places"];
+const mapContainerStyle = {
+  height: "50vh",
+  width: "50vw",
+};
+const options = {
+  styles: mapStyles,
+  disableDefaultUI: true,
+  zoomControl: true,
+};
+const center = {
+  lat: 19.05911471916633,
+  lng: 72.85084258117763,
+};
+
+function index() {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: process.env.API_KEY,
+    libraries,
+  });
+  const [markers, setMarkers] = React.useState([]);
+  const [selected, setSelected] = React.useState(null);
+
+  const onMapClick = React.useCallback((e) => {
+    setMarkers((current) => [
+      ...current,
+      {
+        lat: e.latLng.lat(),
+        lng: e.latLng.lng(),
+        time: new Date(),
+      },
+    ]);
+  }, []);
+  console.log(markers);
+
+  const mapRef = React.useRef();
+  const onMapLoad = React.useCallback((map) => {
+    mapRef.current = map;
+  }, []);
+
+  if (loadError) return "Error";
+  if (!isLoaded) return "Loading...";
+
+  return (
+    <div>
+      <GoogleMap
+        id="map"
+        mapContainerStyle={mapContainerStyle}
+        zoom={8}
+        center={center}
+        options={options}
+        onClick={onMapClick}
+        onLoad={onMapLoad}
+      >
+        {markers.map((marker) => (
+          <Marker
+            key={`${marker.lat}-${marker.lng}`}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            onClick={() => {
+              setSelected(marker);
+            }}
+            icon={{
+              // url: `/bear.svg`,
+              origin: new window.google.maps.Point(0, 0),
+              anchor: new window.google.maps.Point(15, 15),
+              scaledSize: new window.google.maps.Size(30, 30),
+            }}
+          />
+        ))}
+
+        {selected ? (
+          <InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          >
+            <div>
+              <h2>Alert</h2>
+              <p>Spotted {formatRelative(selected.time, new Date())}</p>
+            </div>
+          </InfoWindow>
+        ) : null}
+      </GoogleMap>
+    </div>
+  );
+}
+
+export default index;
